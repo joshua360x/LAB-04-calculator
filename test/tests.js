2db954087d8156ffe46ba886ed62cbf6065899b6
joshua360x <jkw5499@psu.edu>
2021-10-07 14:15:14 -0400

    Added an add test case

--- test/tests.js
+++ test/tests.js
@@ -3,17 +3,41 @@
 
 const test = QUnit.test;
 
-// name your test by what it is testing
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+function add(num1, num2) {
+    return num1 + num2;
+}
 
-    //Expect
+// name your test by what it is testing
+test('adds two numbers 4 and 3', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 4;
+    const y = 3;
+    const expected = 7;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+// name your test by what it is testing
+test('adds two numbers 5 and 3', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 3;
+    const expected = 8;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y); // use your function here
+
+    // 3. Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });

396ff53a638585624d6dc5deb27401d80e889b3e
joshua360x <jkw5499@psu.edu>
2021-10-07 16:43:26 -0400

    Basic Add, Subtract, Multiply, and Divide function working and staged

--- app.js
+++ app.js
@@ -1,18 +1,43 @@
 // import functions
-import { add } from './calculations.js';
+import { add, subtract, multiply, divide } from './calculations.js';
 // console.log(add(50, 50));
 // reference needed DOM elements
 
 
+// Add DOM
 const addNumber1 = document.getElementById('add-number-1');
 const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
 
+
+// Subtract DOM
+const subtractNumber1 = document.getElementById('subtract-number-1');
+const subtractNumber2 = document.getElementById('subtract-number-2');
+const subtractButton = document.getElementById('subtract-button');
+const subtractAnswer = document.getElementById('subtract-answer');
+
+
+// Multiply DOM
+const multiplyNumber1 = document.getElementById('multiply-number-1');
+const multiplyNumber2 = document.getElementById('multiply-number-2');
+const multiplyButton = document.getElementById('multiply-button');
+const multiplyAnswer = document.getElementById('multiply-answer');
+
+
+// Divide DOM
+const divideNumber1 = document.getElementById('divide-number-1');
+const divideNumber2 = document.getElementById('divide-number-2');
+const divideButton = document.getElementById('divide-button');
+const divideAnswer = document.getElementById('divide-answer');
+
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
+
+    
+    // Add event Listener 
 addButton.addEventListener('click', () => {
     const num1 = +addNumber1.value;
     const num2 = +addNumber2.value;
@@ -21,6 +46,42 @@ addButton.addEventListener('click', () => {
     const sum = add(num1, num2);
 
     addAnswer.textContent = sum;
+
+});
+
+    // Subtract event Listener 
+subtractButton.addEventListener('click', () => {
+    const num1 = +subtractNumber1.value;
+    const num2 = +subtractNumber2.value;
+
+
+    const sum = subtract(num1, num2);
+
+    subtractAnswer.textContent = sum;
+
+});
+
+    // Multiply event Listener 
+multiplyButton.addEventListener('click', () => {
+    const num1 = +multiplyNumber1.value;
+    const num2 = +multiplyNumber2.value;
+
+
+    const sum = multiply(num1, num2);
+
+    multiplyAnswer.textContent = sum;
+
+});
+
+    // Divide event Listener 
+divideButton.addEventListener('click', () => {
+    const num1 = +divideNumber1.value;
+    const num2 = +divideNumber2.value;
+
+
+    const sum = divide(num1, num2);
+
+    divideAnswer.textContent = sum;
 
 });
 
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,12 @@
 // IMPORT MODULES under test here:
-import { add } from '../calculations.js';
+import { add, subtract, multiply, divide } from '../calculations.js';
 
 const test = QUnit.test;
 
+// Add
 
 // name your test by what it is testing
-test('adds two numbers 4 and 3', (expect) => {
+test('Adds two numbers 4 and 3', (expect) => {
     // 1. Arrange
     // Set up your arguments and expectations
     const x = 4;
@@ -23,7 +24,7 @@ test('adds two numbers 4 and 3', (expect) => {
 
 
 // name your test by what it is testing
-test('adds two numbers 5 and 3', (expect) => {
+test('Adds two numbers 5 and 3', (expect) => {
     // 1. Arrange
     // Set up your arguments and expectations
     const x = 5;
@@ -38,3 +39,129 @@ test('adds two numbers 5 and 3', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+
+
+
+
+// Subtract 
+
+// name your test by what it is testing
+test('Subtracts two numbers 5 and 3', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 3;
+    const expected = 2;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+// name your test by what it is testing
+test('Subtracts two numbers 10 and 3', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 10;
+    const y = 3;
+    const expected = 7;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = subtract(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+
+// Multiply
+
+// name your test by what it is testing
+test('Multiplies two numbers 5 and 5', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 5;
+    const expected = 25;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+// name your test by what it is testing
+test('Multiplies two numbers 5 and 3', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 3;
+    const expected = 15;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = multiply(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+
+
+// Divide
+
+// name your test by what it is testing
+test('Divides two numbers 5 and 5', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 5;
+    const expected = 1;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+
+// name your test by what it is testing
+test('Divides two numbers 5 and 1', (expect) => {
+    // 1. Arrange
+    // Set up your arguments and expectations
+    const x = 5;
+    const y = 1;
+    const expected = 5;
+    
+    // 2. Act 
+    // Call the function you're testing and set the result to a const
+    const actual = divide(x, y); // use your function here
+
+    // 3. Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
